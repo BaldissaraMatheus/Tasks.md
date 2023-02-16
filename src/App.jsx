6cdfb5945ae728da4a06bdb507b6816b8812ab23
Matheus Baldissara <baldissaramatheus@gmail.com>
2023-02-15 23:03:03 -0300

    Add lanes

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,36 @@ import styles from './App.module.css';
 import ExpandedCard from './components/expanded-card';
 
 function App() {
+  const [lanes, setLanes] = createSignal([
+    { id: 1, name: 'backlog' },
+    { id: 2, name: 'sprint' },
+    { id: 3, name: 'done' },
+  ])
   const [cards, setCards] = createSignal([
     {
       title: null,
       priority: '🔥 High',
       id: 1,
-      content: 'alsdasldjaskldj'
+      content: 'alsdasldjaskldj',
+      laneId: 1,
     },
     {
       title: null,
       priority: '🔔 Medium',
       id: 2,
-      content: 'alsdasldjaskldj'
+      content: 'alsdasldjaskldj',
+      laneId: 2,
     },
     {
       title: null,
       priority: '🤏 Low',
       id: 3,
-      content: 'alsdasldjaskldj'
+      content: 'alsdasldjaskldj',
+      laneId: 2,
     },
   ]);
-  const [cardBeingDraggedIndex, setCardBeingDraggedIndex] = createSignal(null);
-  const [cardToBeReplacedIndex, setCardToBeReplacedIndex] = createSignal(null);
+  const [cardBeingDraggedId, setCardBeingDraggedId] = createSignal(null);
+  const [cardToBeReplacedId, setCardToBeReplacedId] = createSignal(null);
   const [selectedCard, setSelectedCard] = createSignal(null);
   const [cardIdOptionsBeingShown, setCardIdOptionsBeingShown] = createSignal(null);
   const [popupCoordinates, setPopupCoordinates] = createSignal();
@@ -43,20 +51,39 @@ function App() {
     window.removeEventListener('mousedown', handleClickOutsideCardOptions)
   });
 
-  function moveCardPosition() {
+  function moveCardPosition(event) {
+    event.stopPropagation();
     const newCards = structuredClone(cards());
-    const cardBeingDragged = newCards[cardBeingDraggedIndex()];
-    newCards[cardBeingDraggedIndex()] = null;
-    const upOrDownDisplacement = cardBeingDraggedIndex() < cardToBeReplacedIndex()
+    const cardBeingDraggedIndex = newCards.findIndex(card => card.id === cardBeingDraggedId());
+    const cardToBeReplacedIndex = newCards.findIndex(card => card.id === cardToBeReplacedId());
+
+    const cardBeingDraggedLaneId = newCards[cardBeingDraggedIndex].laneId;
+    const cardToBeReplacedLaneId = newCards[cardToBeReplacedIndex].laneId;
+    newCards[cardBeingDraggedIndex].laneId = cardToBeReplacedLaneId;
+    newCards[cardToBeReplacedIndex].laneId = cardBeingDraggedLaneId;
+
+    const cardBeingDragged = newCards[cardBeingDraggedIndex];
+    newCards[cardBeingDraggedIndex] = null;
+    const upOrDownDisplacement = cardBeingDraggedIndex < cardToBeReplacedIndex
       ? 1
       : 0;
     const cardsWithChangedPositions = [
-      ...newCards.slice(0, cardToBeReplacedIndex() + upOrDownDisplacement),
+      ...newCards.slice(0, cardToBeReplacedIndex + upOrDownDisplacement),
       cardBeingDragged,
-      ...newCards.slice(cardToBeReplacedIndex() + upOrDownDisplacement)
+      ...newCards.slice(cardToBeReplacedIndex + upOrDownDisplacement)
     ]
     .filter(card => card !== null);
     setCards(cardsWithChangedPositions);
+  }
+
+  function moveCardToLane(newLaneId) {
+    let newCards = structuredClone(cards());
+    const cardBeingDraggedIndex = newCards.findIndex(card => card.id === cardBeingDraggedId());
+    const newCard = structuredClone(newCards[cardBeingDraggedIndex]);
+    newCard.laneId = newLaneId;
+    newCards = newCards.filter((card, i) => i !== cardBeingDraggedIndex);
+    newCards.push(newCard);
+    setCards(newCards)
   }
 
   function changeCardContent(newContent, cardId) {
@@ -116,30 +143,40 @@ function App() {
             onChange={(value) => changeCardContent(value, selectedCard().id)}
           />
         </Show>
-        <div class={styles.lane}>
-          <For each={cards()} fallback={<div>loading...</div>}>
-            {(card, i) => (
-              <>
-                <div
-                  class={styles.card}
-                  draggable={true}
-                  onDragStart={() => setCardBeingDraggedIndex(i)}
-                  onDragEnd={() => moveCardPosition()}
-                  onDragOver={() => setCardToBeReplacedIndex(i)}
-                  onClick={() => setSelectedCard(card)}
-                >
-                  <div className="toolbar">
-                    <h3 class={styles.h3}>{card.title || 'NO TITLE'}</h3>
-                    <button onClick={event => handleOptionBtnOnClick(event, card.id)}>...</button>
-                  </div>
-                  <div class={styles.chip}>
-                    <h4 class={styles.h4}>{card.priority}</h4>
-                  </div>
-                </div>
-              </>
-            )}
-          </For>
-        </div>
+        <For each={lanes()} fallback={<div>loading...</div>}>
+          {(lane, i) => (
+            <div className="lane-container">
+              <h2>{lane.name}</h2>
+              <div
+                class={styles.lane}
+                onDragOver={() => moveCardToLane(lane.id)}
+              >
+                <For each={cards().filter(card => card.laneId === lane.id)}>
+                  {(card, j) => (
+                    <>
+                      <div
+                        class={styles.card}
+                        draggable={true}
+                        onDragStart={() => setCardBeingDraggedId(card.id)}
+                        onDragEnd={(event) => moveCardPosition(event)}
+                        onDragOver={() => setCardToBeReplacedId(card.id)}
+                        onClick={() => setSelectedCard(card)}
+                      >
+                        <div className="toolbar">
+                          <h3 class={styles.h3}>{card.title || 'NO TITLE'}</h3>
+                          <button onClick={event => handleOptionBtnOnClick(event, card.id)}>...</button>
+                        </div>
+                        <div class={styles.chip}>
+                          <h4 class={styles.h4}>{card.priority}</h4>
+                        </div>
+                      </div>
+                    </>
+                  )}
+                </For>
+              </div>
+            </div>
+          )}
+        </For>
         <Show when={cardIdOptionsBeingShown()}>
           <div
             id={cardIdOptionsBeingShown()}
