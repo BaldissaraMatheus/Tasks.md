4c9cafc6d72f69092294af1dc64545a36b32eed7
Matheus Baldissara <baldissaramatheus@gmail.com>
2024-03-03 17:51:37 -0300

    Replace dragndrop logic with sortable

--- frontend/src/components/lane.jsx
+++ frontend/src/components/lane.jsx
@@ -7,30 +7,53 @@ import {
   createMemo,
 } from "solid-js";
 
+import Sortable from "sortablejs";
+
 /**
  *
  * @param {Object} props
  * @param {string} props.name
- * @param {Object[]} props.cards
- * @param {Function} props.onDragStart
- * @param {Function} props.onDragOver
- * @param {Function} props.onDragEnd
  * @param {Function} props.validateNewName Function that checks if the new typed name is valid; It should return true if it's valid and false if it's not
  * @param {boolean} props.isBeingDraggedOver
  * @param {boolean} props.onRename
  * @param {boolean} props.onDelete
  */
 export function Lane(props) {
+  const [sortable, setSortable] = createSignal(null);
+
+  onMount(() => {
+    const el = document.getElementById(`lane-${props.name}-sortable-container`);
+    if (!el) {
+      return;
+    }
+    setSortable(
+      Sortable.create(el, {
+        animation: 150,
+        group: "tasks",
+        fallbackOnBody: true,
+        swapThreshold: 0.65,
+        onEnd: props.onCardsSortChange,
+      })
+    );
+  });
+
+  onCleanup(() => {
+    if (sortable()) {
+      sortable().destroy();
+    }
+  });
+
   return (
     <>
       <div
-        class={`lane ${props.isBeingDraggedOver ? "dragged-over" : ""}`}
-        onDragEnter={(e) => e.preventDefault()}
-        onDragEnd={props.onDragEnd}
-        onDragOver={props.onDragOver}
+        // class={`lane ${props.isBeingDraggedOver ? "dragged-over" : ""}`}
+        id={`lane-${props.name}`}
+        class="lane"
       >
         <header class="lane__header">{props.headerSlot}</header>
-        <div class="lane__content">{props.children}</div>
+        <div class="lane__content" id={`lane-${props.name}-sortable-container`}>
+          {props.children}
+        </div>
       </div>
     </>
   );
