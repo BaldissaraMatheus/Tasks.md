97630a05d509efd30a3d3cd21827f9ccd461dab1
Matheus Baldissara <baldissaramatheus@gmail.com>
2024-07-25 15:59:39 -0300

    Prevent drag whem clicking name input

--- frontend/src/components/name-input.jsx
+++ frontend/src/components/name-input.jsx
@@ -13,6 +13,7 @@ import { handleKeyDown, clickOutside } from "../utils";
  */
 export function NameInput(props) {
   onMount(() => {
+    // TODO use ref instead
     const input = document.getElementById('rename-input');
     input.focus();
     input.setSelectionRange(0, props.value.length);
@@ -26,17 +27,22 @@ export function NameInput(props) {
     props.onConfirm();
   }
 
+  function handleClick(e) {
+    e.stopPropagation()
+  }
+
   return (
     <div class="input-and-error-msg">
       <input
         type="text"
         id="rename-input"
+        class={props.errorMsg ? "error" : ""}
         value={props.value}
         onInput={(e) => props.onChange(e.target.value)}
         onFocusOut={handleConfirm}
         use:clickOutside={handleConfirm}
         onKeyDown={e => handleKeyDown(e, handleConfirm, props.onCancel)}
-        class={props.errorMsg ? "error" : ""}
+        onClick={handleClick}
       />
       {props.errorMsg ? <span class="error-msg">{props.errorMsg}</span> : <></>}
     </div>
